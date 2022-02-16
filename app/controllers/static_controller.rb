f6fd0d00eb76d4b0c6c4315908b5fd413a7a9b3e
onix <alexander@sautkin.com>
2022-02-17 00:54:32 +0200

    Minor changes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,6 @@ class StaticController < ApplicationController
   def home
     @quote = Quote.order(Arel.sql('RAND()')).first
   end
+
+  def authors; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :quotes
   resources :posts
+  get 'static/authors'
   get 'static/home'
 
   root "static#home"
